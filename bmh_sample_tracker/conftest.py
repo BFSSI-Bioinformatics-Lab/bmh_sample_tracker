0585ab0b635e9f93d7c76d66f611d011d68b57df
Kate Cook <kate.cook@gmail.com>
2023-05-10 14:14:52 -0400

    initial commit

---
+++ bmh_sample_tracker/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bmh_sample_tracker.users.models import User
+from bmh_sample_tracker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
