ff33e09a41bf8d239be2fc59056a6245490b6451
Kate Cook <kate.cook@gmail.com>
2023-05-15 14:36:37 -0400

    get the tests running

--- bmh_sample_tracker/sample_database/apps.py
+++ bmh_sample_tracker/sample_database/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SampleDatabaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "sample_database"
+    name = "bmh_sample_tracker.sample_database"
--- config/settings/base.py
+++ config/settings/base.py
@@ -80,6 +80,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "bmh_sample_tracker.users",
     # Your stuff: custom apps go here
+    "bmh_sample_tracker.sample_database.apps.SampleDatabaseConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
