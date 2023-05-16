d3f517765ca53f93e1a9a0ca989145671bf6d0ab
Kate Cook <kate.cook@gmail.com>
2023-05-16 12:01:33 -0400

    show the project name

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Lab, Sample
+from .models import Lab, Project, Sample
 
 
 class LabSerializer(serializers.ModelSerializer):
@@ -9,8 +9,15 @@ class LabSerializer(serializers.ModelSerializer):
         fields = ["id", "lab_name"]
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ["id", "project_name"]
+
+
 class SampleSerializer(serializers.ModelSerializer):
     submitting_lab = LabSerializer()
+    submitter_project = ProjectSerializer(allow_null=True, required=False)
 
     class Meta:
         model = Sample
