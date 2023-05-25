716ea83ee4c810331bd2d6e484850adf84bdf987
Kate Cook <kate.cook@gmail.com>
2023-05-25 09:56:26 -0400

    add workflowexecution to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Aliquot, Batch, Lab, Project, Sample, Workflow
+from .models import Aliquot, Batch, Lab, Project, Sample, Workflow, WorkflowExecution
 
 admin.site.register(Lab)
 admin.site.register(Project)
@@ -8,3 +8,4 @@ admin.site.register(Sample)
 admin.site.register(Batch)
 admin.site.register(Aliquot)
 admin.site.register(Workflow)
+admin.site.register(WorkflowExecution)
