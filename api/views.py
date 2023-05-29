d6f3cda58abbf7e880572d54a196707db7323847
Kate Cook <kate.cook@gmail.com>
2023-05-29 14:08:04 -0400

    add most recent workflow execution to sample list view

--- api/views.py
+++ api/views.py
@@ -11,7 +11,9 @@ class SampleAPIView(LoginRequiredMixin, APIView):
         if request.user.is_superuser or request.user.is_staff:
             samples = Sample.objects.all()
         else:
-            user_labs = request.user.groups.all().values_list("id", flat=True)  # Retrieve user's lab group names
+            user_labs = request.user.groups.all().values_list("id", flat=True)  # Retrieve user's lab group ids
             samples = Sample.objects.filter(submitting_lab__in=user_labs)
+
         serializer = SampleSerializer(samples, many=True)
+
         return Response(serializer.data)
