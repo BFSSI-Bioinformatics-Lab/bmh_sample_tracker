ec3d307fb105b0f12617f11ea2edff4a7b2ab7af
Kate Cook <kate.cook@gmail.com>
2023-07-21 12:59:49 -0400

    add IP to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com", "microstream.scdc-bio.ca"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["example.com", "127.0.0.1", "192.168.8.14", "microstream.scdc-bio.ca"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
